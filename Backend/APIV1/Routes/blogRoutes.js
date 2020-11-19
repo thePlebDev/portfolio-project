bbfe7bc6a64e031bb89a33e3130144049b0384fa
Tristan Elliott <tristanelliott7@gmail.com>
2020-11-19 16:56:50 -0400

    adding endpoint for single pages

--- Backend/APIV1/Routes/blogRoutes.js
+++ Backend/APIV1/Routes/blogRoutes.js
@@ -40,4 +40,12 @@ blog.get('/post',ensureAuthentication,function(req,res){
   res.json({status:200,message:'we have access to this route'})
 })
 
+blog.post('/single', async (req,res,next)=>{
+  const {id} = req.body
+    let posts =  await BlogPost.findById(id).exec().catch(next)
+    res.json({status:200,posts})
+
+
+})
+
 module.exports = blog
