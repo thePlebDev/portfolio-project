f0f7418240d0c503f4e32b96a45f90bcd185bc95
Tristan Elliott <tristanelliott7@gmail.com>
2020-11-21 13:42:55 -0400

    adding the markdown display

--- src/Components/ArticleCardIndiv/index.js
+++ src/Components/ArticleCardIndiv/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import ReactMarkdown from 'react-markdown'
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+
 import useApiCall from '../../Hooks/useApiCall'
 import axiosUtil from '../../utils/AxiosUtil'
 
 const Container = styled.div`
-  border:1px solid red;
   width:70%;
   margin:0 auto;
 `
@@ -13,12 +16,36 @@ const Container = styled.div`
 
 const ArticleCardIndiv = ({match})=>{
 let a = match.params.id
-const {state,setState} = useApiCall(axiosUtil.get,)
+const title = "Object Oriented Programming"
+const input = '# Object Oriented Programming \n\n and this is a paragraph'
+//const {state,setState} = useApiCall(axiosUtil.get,)
 // it needs an axios function and a location
+const renderers = {
+  code:({language,value}) =>{
+    return <SyntaxHighlighter style={dark} language={language} children={value} />
+  }
+}
+const another = `'
+Below should demostrate how the console.log() method
+~~~js
+console.log('this should look funky')
+~~~
+
+## title
+'`
+
+const markdown = `
+  Below should demostrate how the console.log() method
+  ~~~js
+  console.log('this should look funky')
+  ~~~
+
+  ## title
+`
 
   return(
     <Container>
-      it do be like that sometimes
+      <ReactMarkdown renderers={renderers} children={markdown} />
     </Container>
   )
 }
