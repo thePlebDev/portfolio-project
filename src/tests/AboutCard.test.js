de60a95f589b0fd026264b1631d83a606dfc9281
Tristan Elliott <tristanelliott7@gmail.com>
2020-11-22 13:17:11 -0400

    setting up jest and enzyme

---
+++ src/tests/AboutCard.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme'
+
+import AboutCard from '../Components/AboutCard'
+
+describe('app',()=>{
+  it('renders stuff',()=>{
+    expect(2).toEqual(2)
+  })
+})
