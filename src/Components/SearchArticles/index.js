fff91214125c04bd652900bea92d55740536f505
Tristan Elliott <tristanelliott7@gmail.com>
2020-11-27 15:46:22 -0400

    search stylings

---
+++ src/Components/SearchArticles/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Container = styled.div`
+  text-align:center;
+`
+const Title = styled.div`
+  font-size:1.3rem;
+  color:rgb(30, 130, 87);
+  cursor:pointer;
+`
+
+const Description = styled.div`
+  color:rgba(0, 0, 0, 0.75);
+`
+const Filter = styled.div`
+  color:rgb(30, 130, 87);
+  font-size:0.85rem;
+  margin-bottom:10px;
+  margin-top:10px;
+  cursor:pointer;
+`
+
+const SearchArticles =({title,description,filters})=>{
+
+  return(
+    <Container >
+        <Title>
+          {title}
+        </Title>
+        <Filter>
+          {filters}
+        </Filter>
+        <Description>
+          {description}
+        </Description>
+    </Container>
+  )
+}
+
+export default SearchArticles
--- src/Components/SearchResults/index.js
+++ src/Components/SearchResults/index.js
@@ -1,18 +1,25 @@
 import React,{useEffect,useState} from 'react'
 import styled from 'styled-components'
 
-const Container = styled.div`
+import SearchArticles from '../SearchArticles'
 
+const Container = styled.div`
   border:1px solid green;
-  margin-top:5px
+  margin-top:5px;
+  background-color:#FDFDFD;
 `
 const Articles = styled.div`
   broder:1px solid blue;
   background-color:#FDFDFD;
-
+  display:grid;
+  grid-template-columns:repeat(auto-fit,minmax(350px,1fr));
+  grid-gap:2rem;
 `
-
-
+const Text = styled.div`
+  margin-left:10%;
+  font-size:1.5rem;
+  margin-bottom:10px;
+`
 
 const SearchResults =()=>{
   const [state,setState] = useState()
@@ -23,29 +30,17 @@ const SearchResults =()=>{
 
   return(
     <Container>
+      <Text>Articles</Text>
         <Articles>
-          Articles
-          <div>
             {
               state
                 ?
                 state.map((item)=>{
-                  return <div key={item.key}>
-                    <div>
-                      {item.title}
-                    </div>
-                    <div>
-                      {item.description}
-                    </div>
-                    <div>
-                      {item.filters}
-                    </div>
-                  </div>
+                  return <SearchArticles key={item.key} title={item.title} description={item.description} filters={item.filters}/>
                 })
                 :
                 ''
             }
-          </div>
         </Articles>
     </Container>
   )
