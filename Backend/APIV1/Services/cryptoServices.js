62ae464c2839bec4ebc1857da779f15441796a5e
Tristan Elliott <tristanelliott7@gmail.com>
2020-12-07 16:23:51 -0400

    basic block chain setup

--- Backend/APIV1/Services/cryptoServices.js
+++ Backend/APIV1/Services/cryptoServices.js
@@ -1,7 +1,7 @@
-
 const sha256 = require('sha256');
 // when we call the api we are going to create a block and add it to the old one
 class Block {
+  // so this is for the individual class
   constructor(index,timestamp,data,prevHash){
     this.index = index;
     this.timestamp = timestamp;
@@ -11,6 +11,25 @@ class Block {
       this.index + this.timeStamp + this.data + this.prevHash
     );
   }
+  clculateHash(){
+    return sha256(this.index + this.previousHash + this.timestamp + this.data )
+  }
 }
 
-const createGenesis =()=> new Block(0,Date.now(),"Genesis Block",'0')
+class Blockchain{
+  constructor(){
+    this.chain = [this.createGenesisBlock()]
+  }
+  createGenesisBlock(){
+    return new Block(0,Date.now(),"genesis block","0")
+  }
+  getLatestBlock(){
+    return this.chain[this.chain.length -1]
+  }
+
+  addBlock(newBlock){
+    newBlock.previousHash = this.getLatestBlock().hash
+    newBlock.hash = newBlock.calculateHash()
+    this.chain.push(newBlock)
+  }
+}
