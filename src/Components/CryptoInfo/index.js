2d10a4fb26a92a7c4f629ca51f0a2fe11ddef17a
Tristan Elliott <tristanelliott7@gmail.com>
2020-12-08 19:06:29 -0400

    market volume and cap

--- src/Components/CryptoInfo/index.js
+++ src/Components/CryptoInfo/index.js
@@ -3,16 +3,16 @@ import styled from 'styled-components'
 
 const Container = styled.div`
    display:grid;
-   align-items:center;
-   grid-template-columns:repeat(auto-fit,minmax(340px,1fr));
+   grid-template-columns:repeat(auto-fit,minmax(340px,2fr));
+   border:1px solid blue;
 `
 
 
 const CryptoContainer = styled.div`
+border:1px solid red;
   display:flex;
   align-items:center;
   justify-content:center;
-  margin-top:20px;
 
 `
 const Title = styled.h1`
@@ -21,16 +21,45 @@ const Title = styled.h1`
   margin-top:0;
 `
 const Symbol = styled.span`
-align-self:flex-end;
+align-self:center;
+margin-top:7px;
 margin-bottom:7px;
 margin-left:7px;
 opacity:0.7;
 `
 
-const Price = styled.span
-`
+const Price = styled.span`
   font-size:25px;
 `
+const PriceContainer = styled.div`
+  display:flex;
+  align-items:center;
+  align-self:center;
+  border:1px solid blue;
+  justify-content:center;
+`
+
+const ListContainer = styled.ul`
+border:1px solid blue;
+display:flex;
+align-items:center;
+justify-content:center;
+list-style:none;
+padding-left:0;
+`
+
+const ListItem = styled.li`
+
+  margin-bottom:24px;
+  font-size:15px;
+`
+const H5 = styled.h5`
+  background:rgb(245, 247, 248);
+  padding:12px;
+  color:black;
+
+`
+
 
 const CryptoInfo =({logo,name,symbol,price})=>{
 
@@ -43,10 +72,39 @@ const CryptoInfo =({logo,name,symbol,price})=>{
           <Symbol>({symbol})</Symbol>
       </CryptoContainer>
 
-      <div style={{textAlign:'center',marginTop:'20px'}}>
+      <PriceContainer>
         <Price>${price}</Price>
         <Symbol>USD</Symbol>
-      </div>
+      </PriceContainer>
+
+        <ListContainer>
+          <ListItem>
+            <H5>Market cap</H5>
+            <div style={{padding:'0 12px'}}>
+              <span>3500000000 USD</span>
+              <br/>
+              <span> 18,343,343 BTC</span>
+            </div>
+          </ListItem>
+          <ListItem>
+            <H5>volume</H5>
+            <div style={{padding:'0 12px'}}>
+              <span>3500000000 USD</span>
+              <br/>
+              <span> 18,343,343 BTC</span>
+            </div>
+          </ListItem>
+          <ListItem>
+            <H5>volume</H5>
+            <div style={{padding:'0 12px'}}>
+              <span>3500000000 USD</span>
+              <br/>
+              <span> 18,343,343 BTC</span>
+            </div>
+          </ListItem>
+
+        </ListContainer>
+
 
       <div>
 
