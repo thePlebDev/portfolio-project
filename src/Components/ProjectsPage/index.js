f61cd0432180c587f3874f6b88b23db87f5deb51
Tristan Elliott <>
2021-01-07 23:22:35 -0400

    starting portfolio page

--- src/Components/ProjectsPage/index.js
+++ src/Components/ProjectsPage/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import styled from 'styled-components'
+import dogSpotter from '../../Assets/pupFinderPortFolio.png'
 
 const Container = styled.div`
-
     position:relative;
 `
+
+const Projects = styled.div`
+  width:60%;
+  margin:60px auto;
+  border:1px solid;
+  height:300px;
+  display:grid;
+  align-items:center;
+  grid-template-columns:repeat(auto-fit,minmax(240px, 1fr))
+
+`
+
+const Image = styled.img`
+  border:1px solid red;
+  height:100%;
+  width:100%;
+`
+
+
+
+
 
 
 
@@ -12,7 +33,10 @@ const ProjectsPage =()=>{
 
   return(
     <Container>
-      <h1>THis is where the projects are going to be</h1>
+      <Projects>
+        <Image src={dogSpotter}/>
+        <Image src={dogSpotter}/>
+      </Projects>
     </Container>
   )
 }
