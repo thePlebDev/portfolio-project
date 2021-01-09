e17e379c5ed476b0d857942b0b111800e9fbcbe4
Tristan Elliott <>
2021-01-08 20:47:48 -0400

    adding projects page

--- src/Components/ProjectsPage/index.js
+++ src/Components/ProjectsPage/index.js
@@ -1,7 +1,10 @@
 import React,{useState,useEffect,useRef} from 'react';
 import styled from 'styled-components'
+import CloseIcon from '@material-ui/icons/Close';
+
 import dogSpotter from '../../Assets/pupFinderPortFolio.png'
 import useHoverHook from '../../Hooks/UseHoverHook'
+
 
 const Container = styled.div`
     position:relative;
@@ -53,13 +56,74 @@ const Title = styled.div`
   cursor:pointer;
 `
 
+const ModalContainer = styled.div`
+  position:fixed;
+  top:0;
+  left:0;
+  width:100%;
+  height:100vh;
+  background-color:rgba(0,0,0,0.6);
+  display:flex;
+  justify-content:center;
+  align-items:center;
+
+`
+const Modal = styled.div`
+  background-color:white;
+  height:30%;
+  width:40%;
+  border-radius:4px;
+  @media only screen and (max-width:600px){
+    height:40%;
+  }
+`
+const ModalTitle = styled.div`
+  margin-bottom:5px;
+  font-size:25pt;
+  line-height:30pt;
+  color:#444;
+`
+const Description = styled.div`
+  color:#c0c0c0;
+  font-weight:300;
+  font-size: 11pt;
+  text-transform:uppercase;
+  padding-bottom:15px;
+  border-bottom:1px solid rgba(0, 0, 0, 0.1);
+`
+const Tech = styled.div `
+  color:#e31b6d;
+`
+const CodeButton = styled.button`
+  background-color:#e31b6d;
+  color:white;
+  font-size:15px;
+  padding:5px 15px;
+  border:none;
+  border-radius:4px;
+  position:absolute;
+  bottom:-110px;
+  left:10px;
+  margin:0 auto;
+  cursor:pointer;
+`
+const Close = styled.div`
+  position:absolute;
+  top:0px;
+  right:10px;
+  display:block;
+  width:30px;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  cursor:pointer;
+`
 
 
 const ProjectsPage =()=>{
   const node = useRef(null)
 
   const {state} = useHoverHook(node)
-
 
   return(
     <Container>
@@ -69,6 +133,19 @@ const ProjectsPage =()=>{
           <Image src={dogSpotter}  state={state}/>
         </ImageContainer>
       </Projects>
+      <ModalContainer>
+        <Modal>
+          <div style={{position:'relative'}}>
+              <ModalTitle>Pup Spotter</ModalTitle>
+              <Description>Dog spotting platform</Description>
+              <Tech>React / Node / Express / Mongo </Tech>
+              <CodeButton>View Code</CodeButton>
+              <Close> <CloseIcon style={{color:'#e31b6d',fontSize:'40px'}}/></Close>
+
+
+          </div>
+        </Modal>
+      </ModalContainer>
     </Container>
   )
 }
